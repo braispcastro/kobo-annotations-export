57d6b04e71617c85d5b478b17b758f4318d18e1c
Brais Castro <braispcastro@gmail.com>
2026-01-20 23:07:21 +0100

    Fix: Force trailing slashes to avoid Nginx port redirect issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// https://astro.build/config
+export default defineConfig({
+    trailingSlash: 'always',
+    build: {
+        format: 'directory'
+    }
+});
