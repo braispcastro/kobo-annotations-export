f87168491c1a228d03a9603429680af2aeed8fac
Brais Castro <braispcastro@gmail.com>
2026-01-20 22:17:03 +0100

    Chore: Remove debug script

--- check_colors.ts
+++
@@ -1,7 +0,0 @@
-import { Database } from "bun:sqlite";
-
-const db = new Database("KoboReader.sqlite", { readonly: true });
-const query = db.query("SELECT DISTINCT Color FROM Bookmark");
-const rows = query.all();
-
-console.log("Distinct Colors found:", rows);
